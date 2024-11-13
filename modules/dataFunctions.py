717071d5195efd48e8e555a2d1974564f7f08088
Armaan Choudhary <67652100+armaan-choudhary@users.noreply.github.com>
2024-11-13 13:56:53 +0530

    Added Teacher Data Functions

--- modules/dataFunctions.py
+++ modules/dataFunctions.py
@@ -39,8 +39,6 @@ def UpdateStudent(id, fName="", lName="", dob="", gender="", address="", pNo="")
     else:
         print("Student not found")
 
-    studInfo = cursor.fetchall()
-
 
 def DeleteStudent(id):
     cursor.execute("DELETE FROM Student WHERE StudentID = ?", (id,))
@@ -53,9 +51,11 @@ def SearchStudentByID(id):
     )
 
     rows = cursor.fetchall()
-
-    for row in rows:
-        print("\nStudent No.", row[0], "\nName :", row[1], row[2])
+    if rows == []:
+        print("No Records Found.")
+    else:
+        for row in rows:
+            print("\nTeacher No.", row[0], "\nName :", row[1], row[2])
 
 
 def SearchStudentByName(n):
@@ -68,10 +68,85 @@ def SearchStudentByName(n):
     )
 
     rows = cursor.fetchall()
+    if rows == []:
+        print("No Records Found.")
+    else:
+        for row in rows:
+            print("\nTeacher No.", row[0], "\nName :", row[1], row[2])
 
-    for row in rows:
-        print("\nStudent No.", row[0], "\nName :", row[1], row[2])
+
+def AddTeacher(fName, lName, dob, gender, address, pNo):
+    cursor.execute(
+        """
+      INSERT INTO Teacher (FirstName, LastName, DateOfBirth, Gender, Address, PhoneNumber)
+      VALUES (?, ?, ?, ?, ?, ?)
+    """,
+        (fName, lName, dob, gender, address, pNo),
+    )
 
 
-conn.commit()
-conn.close()
+def UpdateTeacher(id, fName="", lName="", dob="", gender="", address="", pNo=""):
+    cursor.execute("SELECT * FROM Teacher WHERE TeacherID = ?", (id,))
+    teachInfo = cursor.fetchone()
+
+    if teachInfo:
+        cursor.execute(
+            """
+            UPDATE Teacher
+            SET FirstName = ?, LastName = ?, DateOfBirth = ?, Gender = ?, Address = ?, PhoneNumber = ?
+            WHERE StudentID = ?
+            """,
+            (
+                fName or teachInfo[1],
+                lName or teachInfo[2],
+                dob or teachInfo[3],
+                gender or teachInfo[4],
+                address or teachInfo[5],
+                pNo or teachInfo[6],
+                id,
+            ),
+        )
+    else:
+        print("Student not found")
+
+
+def DeleteTeacher(id):
+    cursor.execute("DELETE FROM Teacher WHERE TeacherID = ?", (id,))
+    print("Deleted !")
+
+
+def SearchTeacherByID(id):
+    cursor.execute(
+        "SELECT TeacherID, FirstName, LastName FROM Teacher WHERE TeacherID = ?", (id,)
+    )
+
+    rows = cursor.fetchall()
+    if rows == []:
+        print("No Records Found.")
+    else:
+        for row in rows:
+            print("\nTeacher No.", row[0], "\nName :", row[1], row[2])
+
+
+def SearchTeacherByName(n):
+    cursor.execute(
+        "SELECT TeacherID, FirstName, LastName FROM Teacher WHERE FirstName = ? OR LastName = ?",
+        (
+            n,
+            n,
+        ),
+    )
+
+    rows = cursor.fetchall()
+    if rows == []:
+        print("No Records Found.")
+    else:
+        for row in rows:
+            print("\nTeacher No.", row[0], "\nName :", row[1], row[2])
+
+
+SearchTeacherByName("Dhruv")
+
+
+# conn.commit()
+# conn.close()
